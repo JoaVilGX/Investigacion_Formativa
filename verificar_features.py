265958d3d5713e8628d2918bb4110ab3fa949a0c
mary2017-code <70446604+mary2017-code@users.noreply.github.com>
2026-01-31 15:32:20 -0500

    Update verificar_features.py

--- verificar_features.py
+++ verificar_features.py
@@ -9,7 +9,7 @@ if hasattr(modelo, 'feature_names_in_'):
 else:
     print("El modelo no guardó los nombres de características.")
     
-# Cargar info_flask.json para ver el orden de features_modelo
+# Cargamos info_flask.json para ver el orden de features_modelo
 import json
 with open('models/info_flask.json', 'r') as f:
     info = json.load(f)
